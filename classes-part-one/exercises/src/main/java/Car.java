d2eb07d39d978303fe20effd8c2527c08a92ca8a
werfelmann <werfelmann@gmail.com>
2024-09-16 16:04:27 -0500

    classes part 1

--- classes-part-one/exercises/src/main/java/Car.java
+++ classes-part-one/exercises/src/main/java/Car.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Car {
+    private int speed = 0;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed < 0) {
+            System.out.println("Speed must be a positive integer.");
+        } else {
+            this.speed = speed;
+        }
+    }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,33 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Menu newMenu = new Menu();
+
+        MenuItem veggieBurger = new MenuItem(9.99, "a tasty blackbean burger", "sandwichs", true);
+        MenuItem greekSalad = new MenuItem(10.99, "fresh veggies, olives, and feta on a bed of lightly dressed greens", "salads", true);
+        MenuItem macAndCheese = new MenuItem(8.99, "elbow pasta with a cheesey sauce", "comfort food", true);
+
+        newMenu.addMenuItem(veggieBurger);
+        newMenu.addMenuItem(greekSalad);
+        newMenu.addMenuItem(macAndCheese);
+
+        printMenu(newMenu);
+
+        System.out.println("Menu last updated on: " + newMenu.getLastUpdated());
     }
+
+    private static void printMenu(Menu newMenu) {
+        ArrayList<MenuItem> items = newMenu.getItems();
+        for (MenuItem item : items) {
+            System.out.println(item.getCategory() + ": " + item.getDescription() + " - $" + item.getPrice() + " (New: " + item.isNew() + ")");
+        }
+    }
+
+
+
 }
