51d0a963e833ac965924dd534ae6812b2a3bab5d
werfelmann <werfelmann@gmail.com>
2024-09-19 19:50:27 -0500

    unit testing studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,69 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void misNestedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][[]"));
+    }
+
+    @Test
+    public void bracketAroundWordsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void bracketsInsideOfWordsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void emptyBracketsBeforeWordsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void unclosedBracketReturnsFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void firstBracketIsClosingReturnsFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void closedThenOpenBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void unevenBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void escapedBracketsAreCaptured() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("\\[LaunchCode]\\"));
+    }
+
+
+
+//    @Test
+//    public void
 }
