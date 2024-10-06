19e810da416fbc1537c246fdc3a8f50b4f802eea
werfelmann <werfelmann@gmail.com>
2024-10-06 11:37:57 -0500

    dynamic form added

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,11 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> eventList = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> eventList = new ArrayList<>();
-        eventList.add("concert");
-        eventList.add("wedding");
-        eventList.add("holiday");
-        eventList.add("graduation");
         model.addAttribute("events", eventList);
         return "events/index";
     }
@@ -26,5 +26,11 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        eventList.add(eventName);
+        return "redirect:/events"; //redirect to the root path
     }
 }
